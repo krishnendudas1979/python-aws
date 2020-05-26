3b4bb0b49060e51575b7e022685b6a62e38282b8
Krishnendu Das <krishnendu.das@aricent.com>
2020-05-26 12:38:57 +0530

    AWS -python code --latest

--- aws_base.py
+++ aws_base.py
@@ -2,8 +2,8 @@ import os
 
 import boto3
 
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAJSKKKXBPD6OEF6PA'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_ACCESS_KEY_ID'] = ''
+os.environ['AWS_SECRET_ACCESS_KEY'] = ''
 env_var = os.environ
 aws_access_key_id_env = env_var['AWS_ACCESS_KEY_ID']
 aws_secret_access_key_env = env_var['AWS_SECRET_ACCESS_KEY']
@@ -11,10 +11,10 @@ aws_secret_access_key_env = env_var['AWS_SECRET_ACCESS_KEY']
 
 class aws_base:
     def __init__(self):
-        self.image_type = 'ami-02d0ea44ae3fe9561'
-        self.instance_type = 't2.micro'
-        self.region_name = 'us-west-2'
-        self.IPv4_CIDR = '172.31.0.0/16'
+        self.image_type = ''
+        self.instance_type = ''
+        self.region_name = ''
+        self.IPv4_CIDR = ''
         self.ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key_id_env,
                                        aws_secret_access_key=aws_secret_access_key_env, region_name=self.region_name)
         self.ec2_resource = boto3.resource('ec2', aws_access_key_id=aws_access_key_id_env,
